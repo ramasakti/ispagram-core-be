8bb69015106650375dc4b5a69caa76e8f14118e5
unknown <ramasakti1337@gmail.com>
2024-08-07 20:10:53 +0700

    Config DB Full dari ENV

--- Config.js
+++ Config.js
@@ -4,9 +4,9 @@ const db = knex({
     client: 'mysql',
     connection: {
         host: process.env.DB_HOST,
-        user: process.env.DB_USERNAME ?? 'root',
-        password: process.env.DB_PASSWORD ?? '',
-        database: process.env.DB_NAME ?? 'parlaungan'
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
     }
 });
 
